d4fd5aa127afc3b844300ceea52731f5d19f87bc
codehzy <1257664828@qq.com>
2022-09-14 15:19:43 +0800

    refactor(格式化代码): 🦄 格式化代码

--- server/src/database/sequelize.ts
+++ server/src/database/sequelize.ts
@@ -1,7 +1,11 @@
-import {Sequelize} from 'sequelize-typescript';
-import db from "../../config/db";
+import { Sequelize } from 'sequelize-typescript';
+import db from '../../config/db';
 
-const sequelize = new Sequelize(db.mysql.database, db.mysql.user, db.mysql.password, {
+const sequelize = new Sequelize(
+  db.mysql.database,
+  db.mysql.user,
+  db.mysql.password,
+  {
     // 主机
     host: db.mysql.host,
     // 自定义端口3306
@@ -10,22 +14,24 @@ const sequelize = new Sequelize(db.mysql.database, db.mysql.user, db.mysql.passw
     dialect: 'mysql',
     // 连接池
     pool: {
-        max: 5, // 连接池中最大连接数量
-        min: 0, // 连接池中最小连接数量
-        acquire: 30000,
-        idle: 10000, // 如果一个线程 10 秒钟内没有被使用过的话，那么就释放线程
+      max: 5, // 连接池中最大连接数量
+      min: 0, // 连接池中最小连接数量
+      acquire: 30000,
+      idle: 10000, // 如果一个线程 10 秒钟内没有被使用过的话，那么就释放线程
     },
     timezone: '+08:00', // 东八区
-})
+  },
+);
 
 // 验证连接数据库
-sequelize.authenticate().then(() => {
-        console.log('数据库连接成功')
-    }
-).catch(err => {
-        console.log('数据库连接失败')
-        throw  err
-    }
-)
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('数据库连接成功');
+  })
+  .catch((err) => {
+    console.log('数据库连接失败');
+    throw err;
+  });
 
 export default sequelize;
--- server/src/secret/dbKey.ts
+++ server/src/secret/dbKey.ts
@@ -1,25 +1,26 @@
 const productionConfig = {
-    mysql: {
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'test',
-        port: 3306,
-        connectionLimit: 10,
-    }
-}
+  mysql: {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'test',
+    port: 3306,
+    connectionLimit: 10,
+  },
+};
 
 const developmentConfig = {
-    mysql: {
-        host: 'localhost',
-        user: 'root',
-        password: 'hzy1314..',
-        database: 'todo',
-        port: 3306,
-        connectionLimit: 10,
-    }
-}
+  mysql: {
+    host: 'localhost',
+    user: 'root',
+    password: 'hzy1314..',
+    database: 'todo',
+    port: 3306,
+    connectionLimit: 10,
+  },
+};
 
-const Openconfig = process.env.NODE_ENV === 'production' ? productionConfig : developmentConfig
+const Openconfig =
+  process.env.NODE_ENV === 'production' ? productionConfig : developmentConfig;
 
 export default Openconfig;
